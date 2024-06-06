4d8bc318f4c5c06b044ae131a57a121f5dc31ab5
noahross <noahr688@gmail.com>
2024-06-06 10:29:15 -0400

    Finished

--- 01-MovingSmile/MovingSmile.py
+++ 01-MovingSmile/MovingSmile.py
@@ -6,13 +6,27 @@ def main():
     pygame.init()
     pygame.display.set_caption("Moving Smile")
     screen = pygame.display.set_mode((640, 480))
+    clock = pygame.time.Clock()
+    x = 0
+    y = 0
+
 
     while True:
         # TODO 4: Set the clock speed to 60 fps
+        clock.tick(45)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             # TODO 3: Make the eye pupils move with Up, Down, Left, and Right keys
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[pygame.K_UP]:
+            y = y - 5
+        if pressed_keys[pygame.K_DOWN]:
+            y = y + 5
+        if pressed_keys[pygame.K_LEFT]:
+            x = x - 5
+        if pressed_keys[pygame.K_RIGHT]:
+            x = x + 5
 
         screen.fill((255, 255, 255))  # white
 
@@ -23,20 +37,20 @@ def main():
 
         pygame.draw.circle(screen, (225, 225, 225), (240, 160), 25)  # white eye
         pygame.draw.circle(screen, (0, 0, 0), (240, 160), 25, 3)  # black outline
-        pygame.draw.circle(screen, (0, 0, 0), (242, 162), 7)  # black pupil
+        pygame.draw.circle(screen, (0, 0, 0), (242 + x, 162 + y), 7)  # black pupil
 
         pygame.draw.circle(screen, (225, 225, 225), (400, 160), 25)  # white eye
         pygame.draw.circle(screen, (0, 0, 0), (400, 160), 25, 3)  # black outline
-        pygame.draw.circle(screen, (0, 0, 0), (398, 162), 7)  # black pupil
+        pygame.draw.circle(screen, (0, 0, 0), (398 + x, 162 + y), 7)  # black pupil
 
-        # TODO 1: Draw a nose
+        # Done 1: Draw a nose
         # Suggestion: color (80,0,0) location (320,245), radius 10
         # API --> pygame.draw.circle(screen, (r,g,b), (x, y), radius, thickness)
-        pygame.draw.circle(screen, (80,0,0) (320,245), 10)
-        # TODO 2: Draw a mouth
+        pygame.draw.circle(screen, (80,0,0) ,(320,245), 10)
+        # Done 2: Draw a mouth
         # Suggestion: color (0,0,0), x 230, y 320, width 180, height 30
         # API --> pygame.draw.rect(screen, (r,g,b), (x, y, width, height), thickness)
-
+        pygame.draw.rect(screen, (0,0,0),(230,320,180,30))
         pygame.display.update()
 
 
